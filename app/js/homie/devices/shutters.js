ddb24708c4940e529a7ca0f6f8c85daa3546a7ee
Nabendu Maiti <nbmaiti@gmail.com>
2016-09-10 21:24:08 +0530

    shutters all level
    Signed-off-by: Nabendu Maiti <nbmaiti@gmail.com>

--- app/js/homie/devices/shutters.js
+++ app/js/homie/devices/shutters.js
@@ -10,7 +10,9 @@ export default class Shutters extends React.Component {
   }
 
   open () {
-    this.refs['device'].setProperty('level', 0);
+      var val = parseInt(this.props.state.current.level)+10;
+	  	val = val <= 100 ? val : val - 10;
+	  this.refs['device'].setProperty('level', val);
   }
 
   half () {
@@ -18,21 +20,23 @@ export default class Shutters extends React.Component {
   }
 
   close () {
-    this.refs['device'].setProperty('level', 100);
+      var val = parseInt(this.props.state.current.level)-10;
+	  	val = val  >= 0 ? val : val+10;
+	  this.refs['device'].setProperty('level', val);
   }
 
   render () {
     return (
       <Device image={typeof this.props.state.current.level === 'undefined' ? undefined : this.props.state.current.level === 100 ? 100 : this.props.state.current.level >= 90 ? 90 : this.props.state.current.level >= 80 ? 80 : this.props.state.current.level >= 70 ? 70 : this.props.state.current.level >= 60 ? 60 : this.props.state.current.level >= 50 ? 50 : this.props.state.current.level >= 40 ? 40 : this.props.state.current.level >= 30 ? 30 : this.props.state.current.level >= 20 ? 20 : this.props.state.current.level >= 10 ? 10 : 0} color='#f1c40f' ref='device' {...this.props}>
         <div className='ui three buttons'>
-          <button className='ui green icon compact button' onClick={() => this.open()}>
-            <i className='chevron up icon'></i>
+          <button className='ui red icon compact button' onClick={() => this.close()}>
+	      	<i className='chevron minus icon'></i>
           </button>
           <button className='ui orange compact button' onClick={() => this.half()}>
-            50%
-          </button>
-          <button className='ui red icon compact button' onClick={() => this.close()}>
-            <i className='chevron down icon'></i>
+ 			50%
+		  </button>
+	      <button className='ui green icon compact button' onClick={() => this.open()}>
+              <i className='chevron plus icon'></i>
           </button>
         </div>
       </Device>
