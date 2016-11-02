a85c223593d0cda0215d5b0866a40e3ce890b8d5
Nabendu Maiti <nbmaiti@gmail.com>
2016-11-03 01:29:23 +0530

    stt node lighta experiment through button
    Signed-off-by: Nabendu Maiti <nbmaiti@gmail.com>

--- app/js/homie/devices/sttnode.js
+++ app/js/homie/devices/sttnode.js
@@ -10,11 +10,20 @@ export default class Sttnode extends React.Component {
     super(props);
   }
 
+  toggle(value)  {
+  	  this.refs['device'].setProperty2('824d4ae0', 'lighta', 'on', value);
+  }
+
 	render () {
 	  return (
       <Device image= { typeof this.props.state.current.out === 'undefined' ? undefined : this.props.state.current.out ? 'mic_rcvd':'mic' } color='#ea6153' ref='device' {...this.props}>
-         <br/>
-		  { typeof this.props.state.current.out != 'undefined' ? this.props.state.current.out : '?' }
+		<div className='value'>
+	  	  { typeof this.props.state.current.out != 'undefined' ? this.props.state.current.out : '?' }
+		</div>
+	   <div className='ui two buttons'>
+	     <button id="linkid" className='ui green compact button' onClick={() => this.toggle('true')} >LightA</button> 	 
+	     <button id="linkid2" className='ui red compact button' onClick={() => this.toggle('false')} >LightA</button> 
+        </div>
 		</Device>
     );
   }
