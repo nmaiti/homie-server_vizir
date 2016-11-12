7006309d3e74ce2b9dc9279e0e82c469503a4d77
Nabendu Maiti <nbmaiti@gmail.com>
2016-11-13 04:03:45 +0530

    add % print in shutter
    Signed-off-by: Nabendu Maiti <nbmaiti@gmail.com>

--- app/js/homie/devices/shutters.js
+++ app/js/homie/devices/shutters.js
@@ -33,11 +33,11 @@ export default class Shutters extends React.Component {
     return (
       <Device image={typeof this.props.state.current.level === 'undefined' ? undefined : this.props.state.current.level === 100 ? 100 : this.props.state.current.level >= 90 ? 90 : this.props.state.current.level >= 80 ? 80 : this.props.state.current.level >= 70 ? 70 : this.props.state.current.level >= 60 ? 60 : this.props.state.current.level >= 50 ? 50 : this.props.state.current.level >= 40 ? 40 : this.props.state.current.level >= 30 ? 30 : this.props.state.current.level >= 20 ? 20 : this.props.state.current.level >= 10 ? 10 : 0} color='#f1c40f' ref='device' {...this.props}>
         <div className='ui three buttons'>
-          <button className='ui red icon compact button' onClick={() => this.close()}>
+		  <button className='ui red icon compact button' onClick={() => this.close()}>
 	      	<i className='chevron minus icon'></i>
           </button>
-          <button className='ui orange compact button' onClick={() => this.half()}>
- 			50%
+          <button className='ui  orange compact button' onClick={() => this.half()}>
+			{this.props.state.current.level}%
 		  </button>
 	      <button className='ui green icon compact button' onClick={() => this.open()}>
               <i className='chevron plus icon'></i>
